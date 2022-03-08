3c0a5d88b1132057fa6b1160a4e98669b053c780
macizomedia <macizomedia@gmail.com>
2022-03-08 19:55:15 +0100

    Payload detail

--- src/reducer_User.ts
+++ src/reducer_User.ts
@@ -9,11 +9,12 @@ Option timestap useagent info from mobile from chorme > data
 */
 
 export type User = { name: string, email: string, password: string, data: any }
-export type UserAction = { type: "login" | "Logout" | "signup" }
+export type UserAction = { type: "login" | "Logout" | "signup", payload: Partial<User> }
 
 export function todos(state: User, action: UserAction) {
     switch (action.type) {
-
+        case 'signup':
+            return state
         default:
             return state
     }
